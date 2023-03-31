fd06f2a89ded75b111c7747df0b7ef363cfef5eb
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-03-31 21:45:46 +0100

    Add overall comment

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+/**
+ * Test IR Remote Control & Output numerical key code to LCD 2004 display
+ * 
+ * IR Receiver is connected to pin 3
+ * 
+ * LCD 2004 display connected to I2C pins
+ * 
+ * Uses following Extension....
+ * 
+ * https://github.com/1010Technologies/pxt-makerbit-lcd2004
+ */
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
     makerbit.showStringOnLcd2004("                    ", makerbit.position2004(LcdPosition2004.Pos61), 20)
     makerbit.showStringOnLcd2004("" + (makerbit.irButton()), makerbit.position2004(LcdPosition2004.Pos69), 20)
@@ -8,6 +19,3 @@ makerbit.clearLcd2004()
 makerbit.showStringOnLcd2004("IR Remote Control", makerbit.position2004(LcdPosition2004.Pos2), 20)
 makerbit.showStringOnLcd2004("Code Display", makerbit.position2004(LcdPosition2004.Pos25), 20)
 basic.showIcon(IconNames.Yes)
-basic.forever(function () {
-	
-})
