50f536fb98db66d307a2f414945f192fe514bf88
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-03-31 21:33:24 +0100

    clear display line each time

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
+    makerbit.showStringOnLcd2004("                    ", makerbit.position2004(LcdPosition2004.Pos41), 20)
     makerbit.showStringOnLcd2004("" + (makerbit.irButton()), makerbit.position2004(LcdPosition2004.Pos41), 20)
 })
-makerbit.onIrDatagram(function () {
-	
-})
 makerbit.connectLcd(39)
-makerbit.connectIrReceiver(DigitalPin.P2, IrProtocol.Keyestudio)
+makerbit.connectIrReceiver(DigitalPin.P2, IrProtocol.NEC)
 makerbit.clearLcd2004()
 makerbit.showStringOnLcd2004("Jason IR Tester", makerbit.position2004(LcdPosition2004.Pos1), 20)
-basic.showIcon(IconNames.Skull)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
 })
