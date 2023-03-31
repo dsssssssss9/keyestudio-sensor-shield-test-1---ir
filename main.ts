0f073546e585fc13704d367152afe18ac2b3b2a1
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-03-31 21:40:48 +0100

    format output to vertical centered

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
-    makerbit.showStringOnLcd2004("                    ", makerbit.position2004(LcdPosition2004.Pos41), 20)
-    makerbit.showStringOnLcd2004("" + (makerbit.irButton()), makerbit.position2004(LcdPosition2004.Pos41), 20)
+    makerbit.showStringOnLcd2004("                    ", makerbit.position2004(LcdPosition2004.Pos61), 20)
+    makerbit.showStringOnLcd2004("" + (makerbit.irButton()), makerbit.position2004(LcdPosition2004.Pos69), 20)
 })
 makerbit.connectLcd(39)
 makerbit.connectIrReceiver(DigitalPin.P2, IrProtocol.NEC)
 makerbit.clearLcd2004()
-makerbit.showStringOnLcd2004("Jason IR Tester", makerbit.position2004(LcdPosition2004.Pos1), 20)
+makerbit.showStringOnLcd2004("IR Remote Control", makerbit.position2004(LcdPosition2004.Pos2), 20)
+makerbit.showStringOnLcd2004("Code Display", makerbit.position2004(LcdPosition2004.Pos25), 20)
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
